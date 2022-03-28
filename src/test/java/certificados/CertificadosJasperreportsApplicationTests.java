a413334c454e9ec6c251c5da1eed993b3fdd45c1
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-03-28 16:50:26 -0300

    Iniciado

---
+++ src/test/java/certificados/CertificadosJasperreportsApplicationTests.java
@@ -0,0 +1,13 @@
+package certificados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificadosJasperreportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
